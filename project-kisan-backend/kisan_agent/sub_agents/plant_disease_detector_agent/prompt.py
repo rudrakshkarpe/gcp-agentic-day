603552c216643348d97ce6dc19f30c353f12bd3f
Arora, Vansh <vansh.arora@united.com>
2025-07-22 00:56:35 +0530

    boiler plate code

---
+++ project-kisan-backend/kisan_agent/sub_agents/plant_disease_detector_agent/prompt.py
@@ -0,0 +1,2 @@
+# This file contains the prompt for the Plant Disease Detector Agent.
+PLANT_DISEASE_DETECTOR_INSTRUCTION = """You are a specialized agent that detects plant diseases from images."""
